1586a7808f9723d01f1019de8e3d66233bc35700
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-04-28 13:38:55 +0000

    varaibles, datatypes and shortcuts

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs ,not browser
+
+console.log(3
+    + 3) // code readability should be high
+
+    console.log("Rudra") 
+
+
+    let name ="rudra"
+    let age = 18
+    let isLoggedIn = false
+    let state; // undefined
+
+    // number => 2 to the power 53
+    // bigint
+    //string => ""
+    //boolean => true/false
+    //null=> srtandalone value
+    //undefined => 
+    // symbol => unique 
+
+    //object
+
+    console.log(typeof undefined); // undefined 
+    console.log(typeof null); // object
+
