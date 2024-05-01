7c249c21aa4f34e9c6b0ae7993b1c591ccdf95c8
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-05-01 09:53:56 +0000

    03_conversion of operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,3 +25,29 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 //console.log(stringNumber);
 //console.log(typeof stringNumber);
+
+// *************************Operations*************************
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+//console.log("1"+ 2); xxxx(should not write code like this)
+//console.log(1 +"2"); xxx
+// console.log("1" + 2 + 2); xxx
+// console.log(1 + 2 + "2"); xxx
+
+// console.log ((3+4) * 5 % 3) use parenthes so the code becomes more readable, don't write confusing code because the person coming next will also need to be able to understand your code
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,  num2,  num3
+
+num1 = num2 = num3 = 2+2 //bad way to assign same value to the variable
+
+let gameCounter = 100
+++gameCounter;  // prefix & postfix 
+console.log(gameCounter);
+
