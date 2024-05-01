8e5f8f5c7ff5daf2b4d69d5dbadf42b6cb2c882d
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-05-01 10:59:40 +0000

    comparision in js

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log( "2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log( null == 0);
+console.log(null >= 0);
+
+console.log( undefined == 0);
+console.log( undefined > 0);
+console.log( undefined < 0);
+
+// conversions like this can be confusing so its better to avoid writing comparisions like this 
+// write simple and clean code only
