7142493176c1aeb64e0205ad893d5bba6dcbf358
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-05-26 10:10:15 +0000

    arrays and interview questions

---
+++ Interview questions/02_basics_interview.js
@@ -0,0 +1,9 @@
+// Shallow Copy copy whose refrences share the same refrences like heap
+// Deep Copy a copy whose refrences do not share the same refrences like stack
+
+// Array Interview questions
+
+// diff btwn slice and splice
+
+// slice doesnot manipulate the original array 
+// splice manipulates the original array
