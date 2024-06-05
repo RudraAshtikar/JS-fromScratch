112e0ba2017c226ff8a080f82651dfc30ff50b00
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-06-05 12:19:28 +0000

    04_objects, objects destructure and JSON API

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,11 +1,13 @@
 // const tinderUser = new object()
-const tinderUser = {}
+const tinderUser = {
+    gender: "male"
+}
 
 tinderUser.id = "123abc"
 tinderUser.name ="Sammy"
 tinderUser.isloggedIn = false
 
-//console.log(tinderUser);
+// console.log(tinderUser);
 
 const regularUser ={
     email: "some@gmail.com",
@@ -24,9 +26,9 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 ={5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2};
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // used to add the objects together in one the first parameter i.e "{}" i.e the target obj 
 
-const obj3 = {...obj1, ...obj2, ...obj4}
+const obj3 = {...obj1, ...obj2, ...obj4} //spread operator
 // console.log(obj3);
 
 
@@ -63,3 +65,18 @@ const course = {
 //course.courseInstructor
 
 const {courseInstructor : instructor}= course
+
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[                   // this is how API codes are returned whenever we asked for them
+    {},
+    {},
+    {}
+]
