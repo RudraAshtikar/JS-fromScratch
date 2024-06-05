ab1b29621fea314fefdaa18ef2acc2f84af43f6d
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-06-05 14:09:50 +0000

    Change name

---
+++ Interview questions/03_basics_interview_ques.js
@@ -0,0 +1,8 @@
+// que1 what goes in the num operator?
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// ...num1 is a rest operator
+
+// console.log((calculateCartPrice(200,400, 500, 2000)));
