bdf7ddf42cc153e0ebce0060f237ba3d0160f04c
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-06-14 09:37:37 +0000

    explaination on this

--- Interview questions/03_basics_interview_ques.js
+++ Interview questions/03_basics_interview_ques.js
@@ -14,3 +14,34 @@ function calculateCartPrice(val1, val2, ...num1){
 // this refers current context
 
 // global object in browser is the window object
+
+/*
+
+different behaviour of this in regular function and arrow function?
+
+    -1->In a regular function, the this keyword refers to the object that the function is called on.
+
+    -1.1-> 
+    function chaii()
+    {
+    let username = "rudra"
+    console.log(this); // here it acts as a global object
+    }
+
+    -2-> In an arrow function, the this keyword refers to the object that the function is defined on.
+
+    -2.1->
+    const chai = () =>{
+    let username ="rudra"
+    console.log(this); // here it retruns empty scope
+}
+*/
+module.exports.someproperty = "somevalue"
+
+const chai = () => {
+    console.log(this);
+}
+
+// chai()
+
+// This shows that when `this` defined in arrow function in global context refers to module.exports
