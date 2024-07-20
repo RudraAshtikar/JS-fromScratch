f6e3bd05042c5c804795b189f4f77922f3afecb5
Rudra Ashtikar <93781694+RudraAshtikar@users.noreply.github.com>
2024-07-20 08:02:27 +0000

    Classes

---
+++ 10_Classes_and_OOP/myClasses.js
@@ -0,0 +1,44 @@
+// Es6 
+
+class user {
+    constructor(username, email, password){
+        this.username = username
+        this.email= email
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("chai", 'chai@gmail.com', '123')
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+// behind the scene as we can do these things without classes aswell
+
+function User(username, email, password){
+    this.username = username
+    this.email= email
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User('tea', 'tea@gmail.com', '123')
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
